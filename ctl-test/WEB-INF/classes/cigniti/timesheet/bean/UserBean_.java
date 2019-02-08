3c889022c1c1ae660ea3ad64a2a8ec510339af49
ctl-timesheet <47352787+ctl-timesheet@users.noreply.github.com>
2019-02-08 13:50:32 +0530

    Add files via upload

---
+++ ctl-test/WEB-INF/classes/cigniti/timesheet/bean/UserBean_.java
@@ -0,0 +1,20 @@
+package cigniti.timesheet.bean;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(UserBean.class)
+public abstract class UserBean_ {
+
+	public static volatile SingularAttribute<UserBean, String> user_status;
+	public static volatile SingularAttribute<UserBean, String> project;
+	public static volatile SingularAttribute<UserBean, String> userEmail;
+	public static volatile SingularAttribute<UserBean, String> userName;
+	public static volatile SingularAttribute<UserBean, Integer> bu_id;
+	public static volatile SingularAttribute<UserBean, String> userId;
+	public static volatile SingularAttribute<UserBean, Integer> du_id;
+
+}
+
---
+++ ctl-test/WEB-INF/classes/cigniti/timesheet/entities/EmpReportto_.java
@@ -0,0 +1,18 @@
+package cigniti.timesheet.entities;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(EmpReportto.class)
+public abstract class EmpReportto_ {
+
+	public static volatile SingularAttribute<EmpReportto, String> copytoEmpNumber;
+	public static volatile SingularAttribute<EmpReportto, String> empNumber;
+	public static volatile SingularAttribute<EmpReportto, String> apprEmpNumber;
+	public static volatile SingularAttribute<EmpReportto, String> reportingMode;
+	public static volatile SingularAttribute<EmpReportto, String> supEmpNumber;
+
+}
+
